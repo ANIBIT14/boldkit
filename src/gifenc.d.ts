59c999865154b3cb1bf785bfe350b29785974786
ANIBIT14 <aniruddha.agarwal18@gmail.com>
2026-05-14 12:53:03 +0530

    chore: remove unused gifenc and react-syntax-highlighter dependencies

--- src/gifenc.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'gifenc' {
-  export function GIFEncoder(): {
-    writeFrame(indexedPixels: Uint8Array, width: number, height: number, options?: { palette?: number[][]; delay?: number; repeat?: number }): void
-    finish(): void
-    bytes(): Uint8Array
-  }
-  export function quantize(rgba: Uint8ClampedArray | Uint8Array, maxColors: number): number[][]
-  export function applyPalette(rgba: Uint8ClampedArray | Uint8Array, palette: number[][]): Uint8Array
-}
